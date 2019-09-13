2c5ec9a127c73b0770fbecb0ba14f475c1a9c2c5
Celso Junior <clsjunnior@live.com>
2019-09-12 23:15:41 -0300

    Finalização da aplicação
    Finalização da aplicação

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -1,14 +1,45 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import { loadLists } from '../../services/api';
 import List from '../List';
 import { Container } from './styles';
+import produce from 'immer';
+import BoardContext from './context';
 
-const lists = loadLists();
+const data = loadLists();
 
 export default function Board() {
+  const [lists, setLists] = useState(data);
+  const [isMovingCard, setIsMovingCard] = useState(false);
+
+  const handleDrop = useCallback(
+    (index, item) => {
+      if(!isMovingCard){
+        setLists(produce(lists, draft => {
+          const dragged = draft[item.listIndex].cards[item.index];
+          draft[item.listIndex].cards.splice(item.index, 1);
+          draft[index].cards.push(dragged);
+        }))
+      }else{
+        setIsMovingCard(false);
+      }
+    },
+  )
+
+  function move(fromList, toList, from, to){
+    console.log(from, to, fromList);
+    setLists(produce(lists, draft =>{
+      const dragged = draft[fromList].cards[from];
+      setIsMovingCard(true);
+      draft[fromList].cards.splice(from, 1);
+      draft[toList].cards.splice(to, 0, dragged);
+    }))
+  }
+
   return (
-    <Container>
-      {lists.map(list => <List key={list.title} data={list} />)}
-    </Container>
+    <BoardContext.Provider value={{ lists, move }}>
+      <Container>
+        {lists.map((list, index) => <List accepts={['CARD']} onDrop={item => handleDrop(index, item)} key={list.title} index={index} data={list} />)}
+      </Container>
+    </BoardContext.Provider>
   );
 }
--- src/components/List/index.js
+++ src/components/List/index.js
@@ -1,24 +1,46 @@
 import React from 'react';
 import { MdAdd } from 'react-icons/md';
+import { DropTarget } from 'react-dnd';
 import { Container } from './styles';
 import Card from '../Card';
 
-export default function List({ data }) {
-  return (
+function List({ data, index: listIndex, connectDropTarget }) {
+
+  return connectDropTarget(
+    <div style={{flexGrow:0,flexShrink:0,flexBasis:320}}>
     <Container done={data.done}>
       <header>
         <h2>{data.title}</h2>
-        {
-          data.creatable && (
+        {data.creatable && (
           <button type="button">
-            <MdAdd size={24} color="#fff"></MdAdd>
+            <MdAdd size={24} color="#FFF" />
           </button>
-          )
-        }
+        )}
       </header>
       <ul>
-        {data.cards.map((card, index) => <Card key={card.id} index={index} data={card} ></Card>)}
+        { data.cards.map((card, index) => (
+          <Card 
+            key={card.id} 
+            listIndex={listIndex}
+            index={index} 
+            data={card}
+          />
+        )) }
       </ul>
     </Container>
+    </div>
   );
 }
+
+export default DropTarget (
+  props => props.accepts,
+  {
+    drop(props, monitor) {
+      props.onDrop(monitor.getItem())
+    },
+  },
+  (connect, monitor) => ({
+    connectDropTarget: connect.dropTarget(),
+    isOver: monitor.isOver(),
+  }),
+)(List); 
